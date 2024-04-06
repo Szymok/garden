1207222f1e40540c5343b39aecb4eb3aed2ee8ce
Szymok <szymonkowal019@gmail.com>
2024-04-06 16:43:42 +0200

    Quartz sync: Apr 6, 2024, 4:43 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "quartz.skszymon.eu",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
