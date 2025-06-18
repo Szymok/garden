cc99077a3fd8c0a9006282e598135b28f4e4d6bb
Szymon <szymonkowal019@gmail.com>
2025-06-18 15:49:19 +0200

    now notes

---
+++ content/quartz/C XACT ABORT.sql
@@ -0,0 +1,178 @@
+﻿/*
+	Tomasz Libera | libera@kursysql.pl
+
+	TRANSAKCJE
+	C: XACT_ABORT
+
+*/
+
+
+USE AdventureWorks2017
+GO
+
+
+SELECT * FROM Production.ProductSubcategory WHERE ProductCategoryID = 1
+
+DROP TABLE IF EXISTS dbo.Products
+
+CREATE TABLE dbo.Products (
+	ID int IDENTITY,
+	ProductSubCategoryID int REFERENCES Production.ProductSubcategory(ProductSubCategoryID),
+	ProductName nvarchar(100)
+)
+
+
+
+
+/*
+	Batch
+	XACT_ABORT OFF (DEFAULT)
+*/
+
+DBCC USEROPTIONS
+
+
+INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (1, 'Epic')
+-- FK Error
+INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (111, 'Epic Hardtail')
+INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (1, 'Chisel')
+GO
+
+SELECT * FROM dbo.Products
+
+
+/*
+	Batch
+	XACT_ABORT ON
+*/
+
+TRUNCATE TABLE dbo.Products
+
+SET XACT_ABORT ON
+GO
+
+DBCC USEROPTIONS
+GO
+
+
+INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (1, 'Epic')
+-- FK Error
+INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (111, 'Epic Hardtail')
+INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (1, 'Chisel')
+GO
+
+SELECT * FROM dbo.Products
+
+
+
+/*
+	Transaction
+	XACT_ABORT OFF (DEFAULT)
+*/
+
+
+TRUNCATE TABLE dbo.Products
+
+SET XACT_ABORT OFF
+GO
+
+DBCC USEROPTIONS
+GO
+
+
+BEGIN TRANSACTION
+
+	INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (1, 'Epic')
+	-- FK Error
+	INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (111, 'Epic Hardtail')
+	INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (1, 'Chisel')
+
+COMMIT
+GO
+
+SELECT * FROM dbo.Products
+
+
+
+
+
+/*
+	Transaction
+	XACT_ABORT ON
+*/
+
+
+TRUNCATE TABLE dbo.Products
+
+SET XACT_ABORT ON
+GO
+
+DBCC USEROPTIONS
+GO
+
+
+BEGIN TRANSACTION
+
+	INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (1, 'Epic')
+	-- FK Error
+	INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (111, 'Epic Hardtail')
+	INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (1, 'Chisel')
+
+COMMIT
+GO
+
+SELECT * FROM dbo.Products
+
+
+
+/*
+	TRY-CATCH
+	XACT_ABORT OFF (DEFAULT)
+*/
+
+TRUNCATE TABLE dbo.Products
+
+SET XACT_ABORT OFF
+GO
+
+DBCC USEROPTIONS
+GO
+
+
+
+BEGIN TRANSACTION
+BEGIN TRY
+	INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (1, 'Epic')
+	-- FK Error
+	INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (111, 'Epic Hardtail')
+	INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (1, 'Chisel')
+
+COMMIT
+END TRY
+BEGIN CATCH
+	ROLLBACK
+	PRINT 'Błąd i wycofanie transakcji';
+	THROW
+END CATCH
+
+SELECT * FROM dbo.Products
+
+
+
+
+-- Wykonanie bez błędu
+BEGIN TRANSACTION
+BEGIN TRY
+	INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (1, 'Epic')
+	INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (1, 'Epic Hardtail')
+	INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) VALUES (1, 'Chisel')
+
+COMMIT
+END TRY
+BEGIN CATCH
+	ROLLBACK
+	PRINT 'Błąd i wycofanie transakcji';
+	THROW
+END CATCH
+
+SELECT * FROM dbo.Products
---
+++ content/quartz/G Snapshot isolation level.sql
@@ -0,0 +1,288 @@
+﻿/*
+	Tomasz Libera | libera@kursysql.pl
+
+	TRANSAKCJE
+	G: Snapshot - migawkowy poziom izolacji transakcji
+
+*/
+
+
+
+USE AdventureWorks2017
+GO
+
+IF(SELECT @@TRANCOUNT)>0
+ROLLBACK
+
+/*	przygotowanie danych  */
+DROP TABLE IF EXISTS dbo.Products
+
+CREATE TABLE dbo.Products (
+	ID int IDENTITY PRIMARY KEY,
+	ProductSubCategoryID int 
+	REFERENCES Production.ProductSubcategory(ProductSubCategoryID),
+	ProductName nvarchar(100)
+)
+
+
+INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) 
+VALUES (1, 'Epic'), (1, 'Epic Hardtail'), (1, 'Chisel')
+INSERT INTO dbo.Products (ProductSubCategoryID, ProductName) 
+VALUES (2, 'Tarmac'), (2, 'Roubaix'), (2, 'Venge')
+
+
+SELECT * FROM dbo.Products
+
+
+
+
+
+/* 
+	Snapshot Isolation Level 
+	- wersje wierszy
+*/
+
+
+DBCC USEROPTIONS
+
+SELECT snapshot_isolation_state, snapshot_isolation_state_desc, is_read_committed_snapshot_on, * 
+FROM sys.databases WHERE database_id = DB_ID()
+
+
+ALTER DATABASE AdventureWorks2017 SET ALLOW_SNAPSHOT_ISOLATION ON
+
+SELECT snapshot_isolation_state, snapshot_isolation_state_desc, is_read_committed_snapshot_on, * 
+FROM sys.databases WHERE database_id = DB_ID()
+
+
+
+SET TRANSACTION ISOLATION LEVEL SNAPSHOT 
+
+DBCC USEROPTIONS
+
+SELECT snapshot_isolation_state, snapshot_isolation_state_desc, is_read_committed_snapshot_on, * 
+FROM sys.databases WHERE database_id = DB_ID()
+
+SELECT COUNT(*) FROM sys.dm_tran_version_store
+
+
+
+SELECT * FROM dbo.Products
+
+
+SELECT @@TRANCOUNT
+
+
+BEGIN TRAN
+  UPDATE dbo.Products SET ProductName = 'Hotrock' -- Epic Hardtail
+  WHERE ID = 2
+  SELECT * FROM dbo.Products
+
+
+--> BEGIN --  Sesja 2
+SET TRANSACTION ISOLATION LEVEL SNAPSHOT 
+
+DBCC USEROPTIONS
+
+BEGIN TRAN
+
+SELECT * FROM dbo.Products
+
+--> END --  Sesja 2
+
+SELECT @@TRANCOUNT AS trancount, COUNT(*) AS cnt 
+FROM sys.dm_tran_version_store
+
+SELECT * FROM sys.dm_tran_version_store
+
+
+
+UPDATE dbo.Products SET ProductName = 'Road: ' + ProductName 
+WHERE ProductSubCategoryID = 2
+
+SELECT * FROM dbo.Products
+
+
+
+SELECT @@TRANCOUNT AS trancount, COUNT(*) AS cnt FROM sys.dm_tran_version_store
+
+SELECT * FROM sys.dm_tran_version_store
+
+
+
+--> BEGIN --  Sesja 2
+
+
+SELECT * FROM dbo.Products
+
+UPDATE dbo.Products SET ProductName = 'Stumpjumper' WHERE ID = 1
+
+
+--> END --  Sesja 2
+
+
+-- druga transakcja - transaction_sequence_num
+SELECT @@TRANCOUNT AS trancount, COUNT(*) AS cnt FROM sys.dm_tran_version_store
+
+SELECT * FROM sys.dm_tran_version_store
+
+
+
+SELECT * FROM dbo.Products
+
+COMMIT 
+
+SELECT * FROM dbo.Products
+
+
+
+--> BEGIN --  Sesja 2
+
+SELECT * FROM dbo.Products
+
+COMMIT
+
+SELECT * FROM dbo.Products
+
+--> END --  Sesja 2
+
+-- Stumpjumper 
+SELECT * FROM dbo.Products
+
+
+
+/* 
+	Snapshot Isolation Level 
+	- modyfikacja tego samego wiersza i wycofanie pierwszej
+*/
+
+
+--> Reset
+
+
+SET TRANSACTION ISOLATION LEVEL SNAPSHOT 
+
+
+DBCC USEROPTIONS
+
+SELECT * FROM dbo.Products
+
+
+BEGIN TRAN
+  UPDATE dbo.Products SET ProductName = 'Hotrock' WHERE ID = 2
+  SELECT * FROM dbo.Products
+
+
+--> BEGIN --  Sesja 2
+
+BEGIN TRAN
+
+SELECT * FROM dbo.Products
+
+UPDATE dbo.Products SET ProductName = 'Hotwalk' WHERE ID = 2
+
+
+--> END --  Sesja 2
+
+
+ROLLBACK
+
+
+
+
+
+
+
+
+/* 
+	Snapshot Isolation Level 
+	- modyfikacja tego samego wiersza i zatwierdzenie pierwszej
+	KONFLIKT
+*/
+
+
+--> Reset
+
+
+SET TRANSACTION ISOLATION LEVEL SNAPSHOT 
+
+
+DBCC USEROPTIONS
+
+SELECT * FROM dbo.Products
+
+
+BEGIN TRAN
+  UPDATE dbo.Products SET ProductName = 'Hotrock' WHERE ID = 2
+  SELECT * FROM dbo.Products
+
+
+--> BEGIN --  Sesja 2
+
+BEGIN TRAN
+
+SELECT * FROM dbo.Products
+
+UPDATE dbo.Products SET ProductName = 'Hotwalk' WHERE ID = 2
+
+
+--> END --  Sesja 2
+
+
+COMMIT
+
+
+
+
+
+
+
+/* 
+	Snapshot Isolation Level 
+	- Niepowtarzalny odczyt (nie występuje)
+
+*/
+
+--> Reset
+
+
+SET TRANSACTION ISOLATION LEVEL SNAPSHOT 
+
+
+DBCC USEROPTIONS 
+
+
+SELECT @@TRANCOUNT
+
+
+--> BEGIN --  Sesja 2
+
+BEGIN TRAN
+
+SELECT * FROM dbo.Products
+
+--> END --  Sesja 2
+
+
+BEGIN TRAN
+UPDATE dbo.Products SET ProductName = 'Hotwalk' WHERE ID = 2
+SELECT * FROM dbo.Products
+
+--> BEGIN --  Sesja 2
+
+SELECT * FROM dbo.Products
+
+--> END --  Sesja 2
+
+
+SELECT * FROM dbo.Products
+
+COMMIT
+
+
+
+
+
+
+
+
